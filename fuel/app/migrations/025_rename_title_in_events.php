bf7291cfe9078df158bbc92faad8c9563749dbc7
Riina K <riinak.tv@gmail.com>
2019-12-22 18:52:32 +0900

    Increased memory limit but not improved, why?

--- fuel/app/migrations/025_rename_title_in_events.php
+++ fuel/app/migrations/025_rename_title_in_events.php
@@ -6,8 +6,6 @@ class Rename_title_in_events
 {
 	public function up()
 	{
-		ini_set('memory_limit', '1G');
-
 		\DBUtil::modify_fields('events', array(
 			'type' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'title'),
 		));
@@ -15,8 +13,6 @@ class Rename_title_in_events
 
 	public function down()
 	{
-		ini_set('memory_limit', '1G');
-
 		\DBUtil::modify_fields('events', array(
 			'title' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'type'),
 		));
