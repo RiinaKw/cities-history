7a0e807742eee25116c10fe82dfb0ae5934218c6
Riina K <riinak.tv@gmail.com>
2019-12-22 21:06:59 +0900

    Fix another migration bug

--- fuel/app/migrations/047_add_new_top_parent_division_id_to_divisions.php
+++ fuel/app/migrations/047_add_new_top_parent_division_id_to_divisions.php
@@ -9,13 +9,13 @@ class Add_new_top_parent_division_id_to_divisions
 		if ( ! \DBUtil::field_exists('divisions', array('new_top_parent_division_id')))
 		{
 			\DBUtil::add_fields('divisions', array(
-				'new_top_parent_division_id' => array('constraint' => 11,  'null' => true, 'type' => 'int', 'after' => 'new_belongs_division_id'),
+				'new_top_parent_division_id' => array('constraint' => 11,  'null' => true, 'type' => 'int', 'after' => 'belongs_division_id'),
 			));
 		}
 		if ( ! \DBUtil::field_exists('divisions', array('new_belongs_division_id')))
 		{
 			\DBUtil::add_fields('divisions', array(
-				'new_belongs_division_id' => array('constraint' => 11,  'null' => true, 'type' => 'int', 'after' => 'parent_division_id'),
+				'new_belongs_division_id' => array('constraint' => 11,  'null' => true, 'type' => 'int', 'after' => 'new_top_parent_division_id'),
 			));
 		}
 	}
