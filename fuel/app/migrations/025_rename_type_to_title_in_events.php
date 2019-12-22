d771189b686acfe3618afbabd95cce91a5cbe03d
Riina K <riinak.tv@gmail.com>
2019-12-23 00:58:08 +0900

    Fix

--- fuel/app/migrations/025_rename_type_to_title_in_events.php
+++ fuel/app/migrations/025_rename_type_to_title_in_events.php
@@ -6,15 +6,21 @@ class Rename_type_to_title_in_events
 {
 	public function up()
 	{
-		\DBUtil::modify_fields('events', array(
-			'type' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'title'),
-		));
+		if ( ! \DBUtil::field_exists('events', array('type')))
+		{
+			\DBUtil::modify_fields('events', array(
+				'type' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'title'),
+			));
+		}
 	}
 
 	public function down()
 	{
-		\DBUtil::modify_fields('events', array(
-			'title' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'type'),
-		));
+		if ( \DBUtil::field_exists('events', array('title')))
+		{
+			\DBUtil::modify_fields('events', array(
+				'title' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'type'),
+			));
+		}
 	}
 }
