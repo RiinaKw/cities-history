27a40aac28b5584a0655371c3180c882457762c6
Riina K <riinak.tv@gmail.com>
2019-12-22 18:48:09 +0900

    Try to allow memory limit to 1G

--- fuel/app/migrations/025_rename_title_in_events.php
+++ fuel/app/migrations/025_rename_title_in_events.php
@@ -6,6 +6,8 @@ class Rename_title_in_events
 {
 	public function up()
 	{
+		ini_set('memory_limit', '1G');
+
 		\DBUtil::modify_fields('events', array(
 			'type' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'title'),
 		));
@@ -13,6 +15,8 @@ class Rename_title_in_events
 
 	public function down()
 	{
+		ini_set('memory_limit', '1G');
+
 		\DBUtil::modify_fields('events', array(
 			'title' => array('constraint' => 100,  'null' => false, 'type' => 'varchar', 'name' => 'type'),
 		));
