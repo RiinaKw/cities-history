f8271cc774803f6d7a86c71d8b89dc81c5490fd0
Riina K <riinak.tv@gmail.com>
2019-12-23 01:08:27 +0900

    Fix

--- fuel/app/migrations/029_add_path_and_path_kana_to_divisions.php
+++ fuel/app/migrations/029_add_path_and_path_kana_to_divisions.php
@@ -6,17 +6,33 @@ class Add_path_and_path_kana_to_divisions
 {
 	public function up()
 	{
-		\DBUtil::add_fields('divisions', array(
-			'path'      => array('constraint' => 200,  'null' => false, 'type' => 'varchar', 'after' => 'fullname_kana'),
-			'path_kana' => array('constraint' => 200,  'null' => false, 'type' => 'varchar', 'after' => 'path'),
-		));
+		if ( ! \DBUtil::field_exists('divisions', array('path')))
+		{
+			\DBUtil::add_fields('divisions', array(
+				'path' => array('constraint' => 200,  'null' => false, 'type' => 'varchar', 'after' => 'fullname_kana'),
+			));
+		}
+		if ( ! \DBUtil::field_exists('divisions', array('path_kana')))
+		{
+			\DBUtil::add_fields('divisions', array(
+				'path_kana' => array('constraint' => 200,  'null' => false, 'type' => 'varchar', 'after' => 'path'),
+			));
+		}
 	}
 
 	public function down()
 	{
-		\DBUtil::drop_fields('divisions', array(
-			'path',
-			'path_kana',
-		));
+		if ( \DBUtil::field_exists('divisions', array('path')))
+		{
+			\DBUtil::drop_fields('divisions', array(
+				'path',
+			));
+		}
+		if ( \DBUtil::field_exists('divisions', array('path_kana')))
+		{
+			\DBUtil::drop_fields('divisions', array(
+				'path_kana',
+			));
+		}
 	}
 }
