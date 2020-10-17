9797dcd900852091799e2cd1ca8b431ce30918a3
Riina K <riinak.tv@gmail.com>
2020-10-17 19:21:52 +0900

    Change develop domain to IP address

--- fuel/app/config/development/config.php
+++ fuel/app/config/development/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-	'base_url'  => 'http://localhost/cities-history-fuel/public/',
+	'base_url'  => 'http://192.168.0.202/cities-history-fuel/public/',
 	'profiling'  => true,
 );
