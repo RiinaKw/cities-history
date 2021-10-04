2b80e0e0defe6909b75bc4b87745b5a66acc3ddd
Riina K <riinak.tv@gmail.com>
2021-10-04 23:50:19 +0900

    config に db.php のサンプルを追加

---
+++ fuel/app/config/test/db-sample.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.2
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2019 Fuel Development Team
+ * @link       https://fuelphp.com
+ */
+
+/**
+ * -----------------------------------------------------------------------------
+ *  Database settings for testing environment
+ * -----------------------------------------------------------------------------
+ *
+ *  This environment is primarily used by unit tests, to run on a
+ *  controlled environment.
+ *
+ *  These settings get merged with the global settings.
+ *
+ */
+
+return array(
+	'default' => array(
+		'connection' => array(
+			'dsn'      => 'mysql:host=localhost;dbname=__db_chp_test__',
+			'username' => '__user__',
+			'password' => '__password__',
+		),
+	),
+);
