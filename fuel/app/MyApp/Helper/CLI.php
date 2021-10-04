b85896cb6adeb8d11698162b7128e4bd2899b18e
Riina K <riinak.tv@gmail.com>
2021-10-04 23:24:37 +0900

    新規ユーザ作成のタスクを修正（パスワード非表示ができない……）

--- fuel/app/MyApp/Helper/CLI.php
+++ fuel/app/MyApp/Helper/CLI.php
@@ -9,20 +9,38 @@ namespace MyApp\Helper;
 class CLI
 {
 	/**
-	 * 標準入力
-	 * @var string
-	 */
-	private const STDIN = 'php://stdin';
-
-	/**
 	 * ユーザからの入力を待つ
 	 * @return string  入力された値
 	 */
 	public static function prompt(): string
 	{
-		$stdin = fopen(static::STDIN, 'r');
-		$string = trim(fgets($stdin, 64));
-		fclose($stdin);
+		$string = trim(fgets(STDIN, 64));
+		return $string;
+	}
+
+	/**
+	 * ポスワードなど画面に表示しないプロンプト
+	 * @return string  入力された値
+	 */
+	public static function promptHidden(): string
+	{
+		if (strncasecmp(PHP_OS, 'WIN', 3) === 0) {
+			// for windows
+			//flock(STDIN, LOCK_EX);
+			//echo "\033[8m";
+			$string = trim(fgets(STDIN, 64));
+			//echo "\033[0m";
+			//flock(STDIN, LOCK_UN);
+		} else {
+			// for linux
+			//system('stty -echo'); // エコーバックをOFFにする
+			//flock(STDIN, LOCK_EX);
+			//echo "\033[8m";
+			$string = trim(fgets(STDIN, 64));
+			//echo "\033[0m";
+			//flock(STDIN, LOCK_UN);
+			//system('stty echo'); // エコーバックをONに戻す
+		}
 		return $string;
 	}
 }
--- fuel/app/tasks/user.php
+++ fuel/app/tasks/user.php
@@ -6,15 +6,55 @@
 
 namespace Fuel\Tasks;
 
+use MyApp\Helper\CLI as MyCLI;
+use MyApp\Helper\CLI\Color;
+use Model_User;
+
 class User
 {
-	public static function create($name, $password)
+	public static function create(/*$name, $password*/)
 	{
+		echo Color::color('==== create a new user ====', 'purple'), PHP_EOL, PHP_EOL;
+
+		do {
+			echo 'enter user name > ';
+			$name = MyCLI::prompt();
+
+			if ($name === '') {
+				echo Color::failure('user name must not be empty, try again'), PHP_EOL;
+			} else {
+				break;
+			}
+		} while (1);
+
+		echo 'enter password > ';
+		$password = MyCLI::promptHidden();
+
+		echo 'enter password again > ';
+		$password_confirm = MyCLI::promptHidden();
+
+		if ($password !== $password_confirm) {
+			echo Color::failure('password missmatch'), PHP_EOL;
+			return 1;
+		}
+
+		$name_str = Color::color($name, 'light_green');
+		$y = Color::color("'y'", 'green');
+		do {
+			echo "user name is {$name_str}. Are you OK? press {$y} to continue > ";
+			$choice = strtolower(MyCLI::prompt());
+
+			if ($choice !== '' && $choice !== 'y') {
+				echo Color::color("canceled to create", 'red'), PHP_EOL;
+				return 1;
+			}
+		} while ($choice !== 'y');
+
 		try {
-			\Model_User::create($name, $password);
-			echo "User created\n";
+			Model_User::createUser($name, $password);
+			echo Color::success("user created"), PHP_EOL;
 		} catch (\Exception $e) {
-			echo $e->getMessage();
+			echo Color::failure($e->getMessage()), PHP_EOL;
 			return;
 		}
 	}
